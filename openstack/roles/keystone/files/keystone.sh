8a5a1d9f61778bd5d2d076942f840d8b56bbff56
Vijayboopathy <vijayboopathy.e@gmail.com>
2016-10-22 18:06:17 +0530

    admin project

--- openstack/roles/keystone/files/keystone.sh
+++ openstack/roles/keystone/files/keystone.sh
@@ -2,5 +2,4 @@
 openstack service create --name keystone --description "OpenStack Identity" identity &&
 openstack endpoint create --region RegionOne identity public http://controller:5000/v2.0 &&
 openstack endpoint create --region RegionOne identity internal http://controller:5000/v2.0 &&
-openstack endpoint create --region RegionOne identity admin http://controller:35357/v2.0 &&
-openstack project create --domain default --description "Admin Project" admin
+openstack endpoint create --region RegionOne identity admin http://controller:35357/v2.0 
